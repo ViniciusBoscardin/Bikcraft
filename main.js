602dd45009ea1ca66fa01ffa328b97ef693e43cb
Vinicius Boscardin <stonervina@gmail.com>
2023-03-22 14:23:22 -0300

    section sobre

---
+++ main.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = sectionTop - windowMetade < 0;
+    if (isSectionVisible) section.classList.add('ativo');
+    else section.classList.remove('ativo');
+  });
+}
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
