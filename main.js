821f3d941df2f20ccbdf6e6e10e74f962d1c36c0
Vinicius Boscardin <stonervina@gmail.com>
2023-03-24 14:44:36 -0300

    responsivo1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const sections = document.querySelectorAll('.js-scroll');
-const windowMetade = window.innerHeight * 0.6;
+const windowMetade = window.innerHeight * 0.9;
 
 function animaScroll() {
   sections.forEach((section) => {
