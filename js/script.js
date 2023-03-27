94e691b374ab2ca83be38885505c1013be273f20
Vinicius Boscardin <stonervina@gmail.com>
2023-03-27 10:53:00 -0300

    sliderHome

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 if (window.SimpleSlide) {
   new SimpleSlide({
     slide: 'quote', // nome do atributo data-slide="principal"
-    time: 5000, // tempo de transição dos slides
+    time: 3000, // tempo de transição dos slides
   });
 
   new SimpleSlide({
